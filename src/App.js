7eb958e9b5f0635b144f0d3637bd4d804d6164fa
Lailson Gabriel <lailsongabrielturbo@gmail.com>
2021-11-26 11:54:30 -0300

    repo atualizado

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { Route, Switch } from "react-router-dom";
+import Dashboard  from "./components/Dashboard";
+import NewProduct from "./components/NewProduct";
+import { MyProvider } from "./context/MyProvider";
 
 function App() {
+
+  useEffect(() => {
+    if(localStorage.getItem('listProducts') === null) {
+      localStorage.setItem('listProducts', '[]');
+    }
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MyProvider>
+      <Switch>
+        <Route exact path="/" component={ Dashboard } />
+        <Route path="/newProduct" component={ NewProduct } />
+      </Switch>
+    </MyProvider>
   );
 }
 
